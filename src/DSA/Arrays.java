30197cf9dcf603a6496ee790a8948026b4c512a5
BamdevBhatta <88645610+BamdevBhatta@users.noreply.github.com>
2023-09-11 14:17:11 +0545

    main

---
+++ src/DSA/Arrays.java
@@ -0,0 +1,21 @@
+package DSA;
+
+public class Arrays {
+public static void main(String []args) {
+	int []marks =  new int[5];
+/*
+ * 1)int []marks;
+ * marks = new int[4]; 
+ * 
+ * 2) int []marks = {23,12,454,676};
+ */
+	
+	marks[0]  = 12;
+	marks[1]  = 233;
+	marks[2]  = 32;
+	marks[3]  = 40;
+	marks[4]  = 45;
+	System.out.println(marks[0]);
+	System.out.println(marks[1]);
+}
+}
---
+++ src/DSA/Linked_List.java
@@ -0,0 +1,7 @@
+package DSA;
+
+public class Linked_List {
+public static void main(String []args) {
+	
+}
+}
